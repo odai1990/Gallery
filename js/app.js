161d2a708b5276f241c25608c4c16c88d1bd37e6
odai1990 <odai22odai78@gmail.com>
2021-04-20 14:53:10 +0300

    lab03

--- js/app.js
+++ js/app.js
@@ -1,47 +1,114 @@
 'use strict';
 let arr = [];
-$.ajax('data/page-1.json').then(peopleData => {
-  peopleData.forEach(val => {
-    let newPerson = new Person(val);
-    newPerson.renderName();
-    arr.push(val.keyword);
+Person.all = [];
+
+function page1() {
+  arr = [];
+  Person.all = [];
+  $('.person-template').remove();
+  $('.dropDown').children().remove();
+  $.ajax('data/page-1.json').then(peopleData => {
+    peopleData.forEach(val => {
+      let newPerson = new Person(val);
+      newPerson.renderName();
+      arr.push(val.keyword);
+    })
+    // $('.person-template').first().remove();
+    arr = [...new Set(arr)];
+    renderOption();
   })
-   $('.person-template').first().remove();
+}
 
-   arr = [...new Set(arr)];
-   renderOption();
-   console.log(arr);
- })
-
+function page2() {
+  arr = [];
+  Person.all = [];
+  $('.person-template').remove();
+  $('.dropDown').children().remove();
+  $.ajax('data/page-2.json').then(peopleData => {
+    peopleData.forEach(val => {
+      let newPerson = new Person(val);
+      newPerson.renderName();
+      arr.push(val.keyword);
+    })
+    // $('.person-template').first().remove();
+    arr = [...new Set(arr)];
+    renderOption();
+  })
+}
 function Person(name) {
   this.title = name.title;
   this.image_url = name.image_url;
   this.description = name.description;
   this.keyword = name.keyword;
   this.horns = name.horns;
+  Person.all.push(this);
 }
- Person.prototype.renderName = function() {
 
-   let info = $('.person-template').first().clone();
-   info.find('span').text(this.title);
-   info.find('img').attr('src', this.image_url);
-   info.find('p').text(this.description);
-   info.find('.p1').text(this.keyword);
-   info.find('div').text(`Horns Number: ${this.horns}`);
-   info.addClass(`${this.keyword}`)
-   $('main').append(info);
- }
+Person.prototype.renderName = function () {
+  let template = $('#neighborhoodTemplate').html();
+  let dataSet = Mustache.render(template, this);
+  $('main').append(dataSet)
+}
 
- const renderOption = function() {
+const renderOption = function () {
+  let Options = $('<option value="" class="" selected disabled>FilterByKeyWord</option>')
+  $('#select').append(Options);
+  arr.forEach((item) => {
+    Options = $('<option class = "option"></option>').text(item);
+    $('#select').append(Options);
+  });
+}
 
-   arr.forEach((item)=> {
-     let Options = $('<option class = "option"></option>').text(item);
-     $('select').append(Options);
-   });
- }
+$('#select').on('change', function () {
+  $('.person-template').hide();
+  let x = `${$(this).val()}`;
+  $(`.${x}`).show();
+})
 
- $('#select').on('change', function(){
-   $('.person-template').hide();
-   let x = `${$(this).val()}`;
-   $(`.${x}`).show();
- })
+
+$('.b1').click(function () {
+  $('.person-template').remove();
+
+  page1();
+})
+$('.b2').click(function () {
+  page2();
+})
+
+page1();
+
+function sort() {
+  Person.all.sort((a, b) => {
+    if (a.horns < b.horns) {
+      return 1;
+    } else if (a.horns > b.horns) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+  console.log(Person.all)
+}
+$('.sort').on('change', function (e) {
+  console.log('dd', e.target.value);
+  if (e.target.value == 'number')
+    sort();
+  else
+    title();
+  $('.person-template').remove();
+  Person.all.forEach(val => {
+    val.renderName();
+  })
+})
+
+function title() {
+  Person.all.sort((a, b) => {
+    if (a.keyword.toUpperCase() < b.keyword.toUpperCase()) {
+      return 1;
+    } else if (a.keyword.toUpperCase() > b.keyword.toUpperCase()) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+}
